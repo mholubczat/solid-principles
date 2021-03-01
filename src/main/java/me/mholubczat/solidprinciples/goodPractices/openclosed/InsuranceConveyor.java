aa65677a542ce2af864b391710dc51a3325d7fa9
m.holubczat <m.holubczat@gmail.com>
2021-03-01 09:28:25 +0100

    add good practice example re open closed principle

---
+++ src/main/java/me/mholubczat/solidprinciples/goodPractices/openclosed/InsuranceConveyor.java
@@ -0,0 +1,4 @@
+package me.mholubczat.solidprinciples.goodPractices.openclosed;
+
+public interface InsuranceConveyor {
+}
---
+++ src/main/java/me/mholubczat/solidprinciples/goodPractices/openclosed/VehicleInsuranceConveyor.java
@@ -0,0 +1,2 @@
+package me.mholubczat.solidprinciples.goodPractices.openclosed;public class VehicleInsuranceConveyor {
+}
